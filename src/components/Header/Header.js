ce63ff192a1bb24e286b00076e7bea42f1188647
gilles <quodbach.gilles@gmail.com>
2023-04-19 16:48:58 +0200

    Header.js OK

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,26 @@
 import React, { useEffect } from "react";
 import s from "./style.module.css";
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 function Header(props) {
-  const url = window.location.pathname;
+  const location = useLocation();
+  const currentLocation = location.pathname;
+  console.log(location.pathname);
+  const url = window.location.href;
   useEffect(() => {
-    console.log("****", url);
+    console.log(url);
   }, [url]);
-  if (url === "/") {
-    return (
-      <div className={s.header_container}>
-        <h1 className={s.header_title}>HRnet</h1>
-        <NavLink className={s.header_link} to={"/list"}>
-          View Current Employees
-        </NavLink>
-      </div>
-    );
-  } else {
-    return (
-      <div className={s.header_container}>
-        <h1 className={s.header_title}>HRnet</h1>
-        <NavLink className={s.header_link} to={"/"}>
-          List
-        </NavLink>
-      </div>
-    );
-  }
+
+  return (
+    <div className={s.header_container}>
+      <h1 className={s.header_title}>HRnet</h1>
+      {currentLocation === "/list" ? (
+        <NavLink to="/">Home</NavLink>
+      ) : (
+        <NavLink to="/list">View Current Employees</NavLink>
+      )}
+    </div>
+  );
 }
 
 export default Header;
